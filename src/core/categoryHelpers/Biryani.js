a333690c231d9ad65b1445316980b7f9ed30012e
CHITIKELA <rajesh.chitikela@dxc.com>
2021-05-16 19:15:20 +0530

    added biryani iyems and curris

--- src/core/categoryHelpers/Biryani.js
+++ src/core/categoryHelpers/Biryani.js
@@ -3,20 +3,18 @@ import "../../styles.css";
  
  
 import Card from "../Card";
-import { getProducts } from "../../core/helper/coreapicalls";
+import { getProducts } from "../helper/coreapicalls";
  
 import { isAutheticated } from "../../auth/helper/index";
 // import DemoCarousel from "./Slider";
 
-export default function Wintercategory() {
+export default function Biryani() {
    
   const {user,token}=isAutheticated();
    
   const [products, setProducts] = useState([]);
    
   const [error, setError] = useState(false);
-   
- 
 
   const loadAllProduct = () => {
     getProducts().then(data => {
@@ -32,17 +30,18 @@ export default function Wintercategory() {
     loadAllProduct();
   }, []);
 
- 
    
   return (
     <div className="row text-center">
-     <div className="row">
-       {products.map((product, index) => {
-        console.log(product);
-        let w = window.innerWidth;
-        if(product.category.name){
+         
+     {products.length>0?( <div className="row">
+
+{products.map((product, index) => {
+  let w = window.innerWidth;
+console.log(product);
+  if(product.category.name){
     console.log(product.category.name)
-   if(product.category.name==="winter"){
+   if(product.category.name==="biryani"){
      return (
       w<768?( <div key={index} className="col-6 mb-4">
              <Card product={product} />
@@ -56,9 +55,10 @@ export default function Wintercategory() {
   else{
     return <p>Loading</p>
   }
-         
-       })}
-     </div>
+ 
+  
+})}
+</div>):"Loading......"}
      
    </div>
   )
--- src/core/categoryHelpers/Rotis.js
+++ src/core/categoryHelpers/Rotis.js
@@ -3,18 +3,20 @@ import "../../styles.css";
  
  
 import Card from "../Card";
-import { getProducts } from "../../core/helper/coreapicalls";
+import { getProducts } from "../helper/coreapicalls";
  
 import { isAutheticated } from "../../auth/helper/index";
 // import DemoCarousel from "./Slider";
 
-export default function Wintercategory() {
+export default function Rotis() {
    
   const {user,token}=isAutheticated();
    
   const [products, setProducts] = useState([]);
    
   const [error, setError] = useState(false);
+   
+ 
 
   const loadAllProduct = () => {
     getProducts().then(data => {
@@ -30,18 +32,17 @@ export default function Wintercategory() {
     loadAllProduct();
   }, []);
 
+ 
    
   return (
     <div className="row text-center">
-         
-     {products.length>0?( <div className="row">
-
-{products.map((product, index) => {
-  let w = window.innerWidth;
-console.log(product);
-  if(product.category.name){
+     <div className="row">
+       {products.map((product, index) => {
+        console.log(product);
+        let w = window.innerWidth;
+        if(product.category.name){
     console.log(product.category.name)
-   if(product.category.name==="summer"){
+   if(product.category.name==="rotis"){
      return (
       w<768?( <div key={index} className="col-6 mb-4">
              <Card product={product} />
@@ -55,10 +56,9 @@ console.log(product);
   else{
     return <p>Loading</p>
   }
- 
-  
-})}
-</div>):"Loading......"}
+         
+       })}
+     </div>
      
    </div>
   )
